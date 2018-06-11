73e2d06de2029364a5c1b0bb86af531d85c0f117
borimoon <boriana@freemail.hu>
2018-06-11 23:44:58 +0200

    basic structure
    menu, layout, grid

---
+++ js/classes.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#classes',
+  data: {
+    classItems: [
+      {title: 'Harmonie tánc', text: 'Az Art Jazz  mozdulatvilágával ismerkedünk, amit a Limón technika elemeivel egészítünk ki. A táncórán a gerinc, a karok és a láb mozdulatainak, összehangolt kecses, elnyújtott formáit, íveit, kapcsoljuk össze.'},
+
+      {title: 'Kortárstánc és kombináció', text: 'Útkeresés, a mozdulatokon belül. Minden elengedett, ejtett, lendített, elmozdulás a térben érzelmi töltést kap, melyben szabadon és játékosan fedezzük fel a test mozgáshatárait. Felszabadult, vidám együttműködés jellemzi a táncórát.'},
+
+      {title: 'Fejlesztés és koreográfia', text: 'Erősítő gyakorlatokkal átmozgatjuk az egész testet, ezután több kombináció és koreográfia követi egymást, amit eltáncolunk és finomítunk.'},
+
+      {title: 'Kortárs duó', text: 'Kontakt tánctechnika, kompozíció, emelések. Megtanuljátok, hogyan dolgozzatok együtt, miként hozzatok létre közös mozdulatokat. Az órán maximum két pár vesz részt. Állandó párokkal dolgozunk, párcsere nincs.'},
+
+      {title: 'Esküvőtánc', text: 'Ha egy igazán különleges koreográfiát táncolnátok el a nagy napon, egy általatok választott zenére, személyre szabottan, olyan mozdulatokból építjük fel a koreográfiát, amiben mindketten jól érzitek magatokat. A keringő mellett, kérésre emeléseket, szép karvezetéseket, meghitt pillanatokat is beépíthetünk a táncotokba.'},
+
+      {title: 'Magánóra', text: 'Ha szeretnél a saját tempódban haladni, vannak ötleteid, amiket megvalósítanál, esetleg felvételire, fellépésre készülsz, lehetőséged van magánórát foglalni. Beszéljünk meg egy időpontot.'},
+
+      {title: 'Ha nehéz választani…', text: '…írj bátran és segítek. Az első táncórádon, megajándékozunk egy plusz alkalommal, hogy két tánctechnikát, vagy fejlesztő táncórát is kipróbálhass. Így biztosan megtalálod azt a mozdulatvilágot, ami hozzád a legközelebb áll. Így az első héten 2 táncórán is részt vehetsz.'}
+    ]
+  }
+});
---
+++ js/menu.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#menu',
+  data: {
+    menuItems: [
+      {name: 'rólam', url: '#about'},
+      {name: 'táncórák', url: '#classes'},
+      {name: 'workshopok', url: '#workshop'},
+      {name: 'előadások', url: '#performance'},
+      {name: 'kapcsolat', url: '#contact'}
+    ]
+  }
+});
